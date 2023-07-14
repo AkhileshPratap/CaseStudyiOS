daf00340e061c06b24d15e8eb8557f105f5071aa
Akhilesh Singh <jss.akhileshpratap@gmail.com>
2023-07-14 17:41:01 +0530

    untracked updted in gitignore

--- Pods/Target Support Files/Pods-CaseStudyiOS/Pods-CaseStudyiOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CaseStudyiOS : NSObject
-@end
-@implementation PodsDummy_Pods_CaseStudyiOS
-@end
--- Pods/Target Support Files/Pods-CaseStudyiOSTests/Pods-CaseStudyiOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CaseStudyiOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_CaseStudyiOSTests
-@end
