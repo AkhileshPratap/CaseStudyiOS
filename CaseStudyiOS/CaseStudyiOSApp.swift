ab37791514c73400750e22fd24e1a2f581acf610
Akhilesh Singh <jss.akhileshpratap@gmail.com>
2023-07-14 16:15:50 +0530

    Initial commit

---
+++ CaseStudyiOS/CaseStudyiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseStudyiOSApp.swift
+//  CaseStudyiOS
+//
+//  Created by AkhileshSingh on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CaseStudyiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
